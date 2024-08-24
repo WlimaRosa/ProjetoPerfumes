3b2dfb18f8f2c0db87d8d044ca4ec47b6a124000
aluno.saolucas <aluno.saolucas@CSL-P5A3226X3A.ensino.ulbra.net>
2024-08-23 21:31:39 -0300

    Primeiro commit 1.0

---
+++ src/br/ulbra/ArrayList/ListaBD.java
@@ -0,0 +1,54 @@
+package br.ulbra.ArrayList;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class ListaBD {
+
+    public ArrayList lista;
+
+    public ListaBD() {
+        lista = new ArrayList();
+    }
+
+    public void salvar(String elemento) {
+        if (elemento.equals("") || elemento.equals(null)) {
+            JOptionPane.showMessageDialog(null, "Insira um nome antes de salvar");
+        } else {
+            lista.add(elemento);
+            JOptionPane.showMessageDialog(null, elemento + " Salvo com sucesso !");
+        }
+    }
+
+    public String listar() {
+        String elementos = "";
+        if (lista.isEmpty()) {
+            return "Não foram lançados perfumes. ";
+        } else {
+            for (int i = 0; i < lista.size(); i++) {
+                elementos += (i+1) + " ° " + lista.get(i) + "\n";
+            }
+            return elementos;
+        }
+    }
+    public void alterar(int i , String novoElemento){
+        if (lista.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Lista vazia!");
+        }else if (i>=lista.size() || i < 0){
+            JOptionPane.showMessageDialog(null, "Perfume não existe!");
+        }else {
+        lista.set(i,novoElemento);
+        JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+    }
+    } 
+    public void excluir(int i ){
+        if (lista.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Lista vazia!");
+        }else if (i>=lista.size() || i < 0){
+            JOptionPane.showMessageDialog(null, "Perfume não existe!");
+        }else {
+        lista.remove(i);
+        JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+    }
+    }
+}
